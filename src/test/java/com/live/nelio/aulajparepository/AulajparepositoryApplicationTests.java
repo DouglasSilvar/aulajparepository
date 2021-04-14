79e1996821acfd8f078b353307815e595c59836a
DouglasSilvar <gogaplay@gmail.com>
2021-04-13 23:13:06 -0300

    Primeiro Commit

---
+++ src/test/java/com/live/nelio/aulajparepository/AulajparepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.live.nelio.aulajparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulajparepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
